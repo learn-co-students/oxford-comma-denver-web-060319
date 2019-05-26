d2b207ca2f3840c82149cd874ec6a86b4204568a
foggybrains <dustin.keyes@gmail.com>
2019-05-26 01:45:14 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    list = array.count
+    if list == 1
+        array.pop
+    elsif list == 2
+        array.join(" and ")
+    else
+        pop_array = array.pop
+        string = array.join(", ") + ", and #{pop_array}"
+    end
 end
