068d16d4238bad6f0a9e058ca5e7cc08062b44cb
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-02 00:30:52 -0600

    Convert an array of strings into a single string
    Practice iterating over and converting array elements into a formatted string

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  else
+    new_string = ""
+    array.each do |string|
+      if string != array[-1]
+        new_string.concat("#{string}, ")
+      else
+        new_string.concat("and #{string}")
+      end
+    end
+    new_string
+   end
 end
